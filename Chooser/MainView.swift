14c24632fccbbf3487c1ab43b986dab01ebf35d9
Soo-Han Lee <martino_lee@naver.com>
2019-06-18 21:09:32 +0800

    Add MainView

---
+++ Chooser/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  Chooser
+//
+//  Created by Soohan Lee on 2019/06/18.
+//  Copyright © 2019 Soohan. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MainView_Previews : PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+#endif
